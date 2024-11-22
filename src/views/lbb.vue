a87ffc0d6f9a0d62cef0fb598b7e17522bfaca4e
JTR4JT <2458638823@qq.com>
2024-11-22 22:33:22 +0800

    权限显示

--- src/views/lbb.vue
+++ src/views/lbb.vue
@@ -1,90 +1,138 @@
 <template>
-    <div>
-      <el-table :data="tables" height="300px" border style="width: 100%,color: #000;" :show-header="false">
-        <el-table-column prop="name" label="项目经理">
-        </el-table-column>
-        <el-table-column prop="allProject" label="项目总数">
-        </el-table-column>
-        <el-table-column prop="okProject" label="已出货项目数"> 
-        </el-table-column>
-        <el-table-column prop="waitProject" label="待出货项目数">
-        </el-table-column>
-        <el-table-column prop="problemProject" label="有问题项目">
-        </el-table-column>
-      </el-table>
-    </div>
-  </template>
-  
-  <script>
+  <div>
+    <el-table
+    ref="multipleTable"
+    :data="tableData"
+    tooltip-effect="dark"
+    style="width: 100%"
+    :row-key="getRowKey"
+    @selection-change="handleSelectionChange">
+    <el-table-column
+      type="selection"
+      width="55">
+    </el-table-column>
+    <el-table-column
+      label="日期"
+      width="120">
+      <template slot-scope="scope">{{ scope.row.date }}</template>
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="id"
+      label="地址"
+      show-overflow-tooltip>
+    </el-table-column>
+  </el-table>
+  <div style="margin-top: 20px">
+    <el-button @click="toggleSelection([tableData[1], tableData[2]])">切换第二、第三行的选中状态</el-button>
+    <el-button @click="getSameMsg()">test</el-button>
+    <el-button @click="toggleSelection()">取消选择</el-button>
+  </div>
+  </div>
+ 
+</template>
+
+<script>
   export default {
     data() {
       return {
-        tables: [
-          {
-            name: '张三',
-            allProject: 10,
-            okProject: 3,
-            waitProject: 3,
-            problemProject: 6
-          },
-          {
-            name: '李四',
-            allProject: 10,
-            okProject: 3,
-            waitProject: 3,
-            problemProject: 6
-          },
-          {
-            name: '王五',
-            allProject: 10,
-            okProject: 3,
-            waitProject: 3,
-            problemProject: 6
-          },
-          {
-            name: '老六',
-            allProject: 10,
-            okProject: 3,
-            waitProject: 3,
-            problemProject: 6
-          },
-          {
-            name: '阿七',
-            allProject: 10,
-            okProject: 3,
-            waitProject: 3,
-            problemProject: 6
-          },
-          {
-            name: '老八',
-            allProject: 10,
-            okProject: 3,
-            waitProject: 3,
-            problemProject: 6
-          }
-        ],
-        currentTableIndex: 0,
-        intervalId: null
-      };
+        tableData: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          id: 1
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          id: 2
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          id: 3
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          id: 4
+        }, {
+          date: '2016-05-08',
+          name: '王小虎',
+          id: 5
+        }, {
+          date: '2016-05-06',
+          name: '王小虎',
+          id: 6
+        }, {
+          date: '2016-05-07',
+          name: '王小虎',
+          id: 7
+        }],
+        multipleSelection: [],
+        allMsg:[],
+        testData:[{
+          name: '王小虎',
+          id: 2
+        },
+        {
+          name: '王小虎',
+          id: 7
+        },
+        {
+          name: '王小虎',
+          id: 1
+        }
+      ]
+      }
     },
-    mounted() {
-      this.startCarousel();
-    },
-    beforeDestroy() {
-      this.stopCarousel();
+    mounted(){
+        this.getSameMsg()
     },
     methods: {
-      startCarousel() {
-        this.intervalId = setInterval(() => {
-          this.currentTableIndex = (this.currentTableIndex + 1) % this.tables.length;
-          // 更新 tables
-          this.tables = this.tables[this.currentTableIndex];
-        }, 5000); // 5000 毫秒 = 5 秒，你可以根据需要调整这个值
+      toggleSelection(rows) {
+      
+    if (this.$refs.multipleTable) {
+        if (rows) {
+            rows.forEach(row => {
+              console.log(row)
+                this.$refs.multipleTable.toggleRowSelection(row);
+            });
+        } else {
+            this.$refs.multipleTable.clearSelection();
+        }
+    }
+    
+},
+getRowKey(row) {
+      return row.id
+    },
+
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
       },
-      stopCarousel() {
-        clearInterval(this.intervalId);
+      getSameMsg()
+      {
+        var arr2 = this.tableData
+        var arr1 = this.testData
+        // 使用filter和some来筛选出在arr1中存在的arr2的元素
+        // var rows = JSON.parse(JSON.stringify(arr2.filter(item2 => {
+        //   return arr1.some(item1 => item1.id === item2.id);
+        // })));
+        // this.allMsg = rows
+          for (const item2 of arr2) {
+          for (const item1 of arr1) {
+            if (item1.id === item2.id) {
+              this.allMsg.push(item2);
+              break;  // 找到相等的就不用继续在arr1里找了，直接进入下一轮循环判断arr2的下一项
+            }
+          }
+      }
+      this.toggleSelection( this.allMsg)
+    // this.allMsg.forEach(row => {
+    //             this.$refs.multipleTable.toggleRowSelection(row,true);
+    //         });
       }
     }
-  };
-  </script>
-  
+  }
+</script>
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,219 +1,33 @@
 <template>
-  <div class="table-box">
-    <!--表头-->
-    <div class="table-th">
-      <div style="width: 60px">排名</div>
-      <div>区域</div>
-      <div>总服务人数</div>
-      <div>2023年</div>
-      <div>2024年</div>
-    </div>
-    <!--表格数据-->
-    <div class="table-tr" ref="tableTr" @mouseenter="pause" @mouseleave="resume">
-      <!--克隆一份数据-->
-      <div
-        class="tr-item"
-        v-for="(item, index) in [...tableData, ...tableData]"
-        :key="index"
-        :class="{ 'even-background': index < 15 ? index % 2 !== 0 : index % 2 === 0 }"
-      >
-        <div class="index-item">
-          <div v-if="item.ranking <= 3" class="tab-index index-bg">
-            {{ item.ranking }}
-          </div>
-          <div v-else class="tab-index">
-            {{ item.ranking }}
-          </div>
-        </div>
-        <div>{{ item.province }}</div>
-        <div>{{ item.totalServiceNumber }}</div>
-        <div>{{ item.yearNumber2 }}</div>
-        <div>{{ item.yearNumber3 }}</div>
-      </div>
-    </div>
-  </div>
+  <el-table
+    ref="multipleTable"
+    :data="tableData"
+  >
+    <el-table-colum type="selection"></el-table-colum>
+    <el-table-column  prop="name" label="姓名"></el-table-column>
+  </el-table>
 </template>
-
 <script>
 export default {
-  name: 'demo2',
   data() {
     return {
-      tableData: [],
-      position: 0,
-      animationId: null,
+      tableData: [
+        { id: 1, name: '张三' },
+        { id: 2, name: '李四' },
+        { id: 3, name: '王五' }
+      ]
+    };
+  },
+  methods: {
+    filterData() {
+      this.tableData = this.tableData.filter(item => item.id > 1);
+      console.log(this.tableData);
+      return this.tableData;
     }
   },
   mounted() {
-    this.getTableData()
-    this.$nextTick(() => {
-      this.animation()
-    })
-  },
-  methods: {
-    /**
-     * 暂停动画。
-     * 此方法通过取消动画帧请求来暂停动画的执行。
-     * 当鼠标移入组件时调用，以暂停动画的播放。
-     */
-    pause() {
-      cancelAnimationFrame(this.animationId) // 取消当前的动画帧请求
-      this.animationId = null // 清空动画帧请求的ID
-      this.isPaused = true // 标记动画为暂停状态
-    },
-    /**
-     * 恢复动画。
-     * 如果动画当前处于暂停状态，此方法会重新启动动画。
-     * 当鼠标移出组件时调用，以恢复动画的播放。
-     */
-    resume() {
-      if (this.isPaused) {
-        // 当动画处于暂停状态时
-        this.animation() // 重新调用动画函数
-        this.isPaused = false // 将动画状态设置为播放
-      }
-    },
-    /**
-     * 启动一个动画，用于滚动表格中的行。
-     * 此函数没有参数和返回值，因为它直接操作DOM并启动一个循环动画。
-     * 动画效果是通过改变行的垂直偏移来实现的。
-     */
-    animation() {
-      // 获取将要进行动画的ul元素
-      let tableTr = this.$refs.tableTr
-      // 获取ul元素的高度，用于计算动画的终点
-      let tableTrHeight = tableTr.offsetHeight
-      /**
-       * 动画循环函数。
-       * 不断调整元素的垂直偏移，创造向下滚动的效果。
-       * 当元素偏移超过其高度的一半时，重置偏移值，实现循环滚动。
-       */
-      const animate = () => {
-        this.position -= 2 // 每次循环减少的偏移量，控制动画速度
-        if (this.position <= -(tableTrHeight / 2)) {
-          this.position = 0 // 当偏移量小于等于负的一半高度时，重置偏移量，准备下一次循环
-        }
-        tableTr.style.transform = `translateY(${this.position}px)` // 应用偏移量到元素的transform属性上，实现动画效果
-        this.animationId = requestAnimationFrame(animate) // 请求下一个动画帧继续执行此函数，保持动画运行
-      }
-      animate() // 初始化动画
-    },
-    getTableData() {
-      const generatedData = []
-      const provinces = [
-        '北京',
-        '上海',
-        '广东',
-        '江苏',
-        '浙江',
-        '山东',
-        '河南',
-        '湖南',
-        '湖北',
-        '四川',
-        '福建',
-        '安徽',
-        '陕西',
-        '重庆',
-        '云南',
-      ]
-      const randomServiceNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
-      for (let i = 0; i < 15; i++) {
-        const province = provinces[Math.floor(Math.random() * provinces.length)]
-        const totalServiceNumber = randomServiceNumber(1000, 10000).toString()
-        const yearNumber2 = randomServiceNumber(500, 5000).toString()
-        const yearNumber3 = randomServiceNumber(1000, 8000).toString()
-
-        generatedData.push({
-          ranking: i + 1,
-          province,
-          totalServiceNumber,
-          yearNumber2,
-          yearNumber3,
-        })
-      }
-
-      this.tableData = generatedData
-    },
-  },
-}
+    // 调用filterData方法，但没有正确更新数据
+    this.filterData();
+  }
+};
 </script>
-
-<style  lang="less" scoped>
-.table-box {
-  background-color: #000;
-  color: white;
-  // 最外层父元素必须要定高
-  width: 500px;
-  height: 385px;
-  overflow: hidden;
-}
-
-.table-th {
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  background-color: #1a8dec;
-  font-weight: 100;
-  font-size: 14px;
-  padding: 2px 0;
-  height: 35px;
-  line-height: 35px;
-  position: sticky;
-  z-index: 2;
-
-  & > div {
-    width: 120px;
-    text-align: center;
-  }
-}
-
-.table-tr {
-  display: flex;
-  flex-direction: column;
-
-  .tr-item {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    font-weight: 100;
-    font-size: 12px;
-    height: 35px; // 每一行的高度也必须固定，用户计算滚动距离
-    line-height: 35px;
-
-    & > div {
-      width: 120px;
-      text-align: center;
-    }
-
-    .index-item {
-      width: 60px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-  }
-
-  //.tr-item:nth-child(odd) {
-  //  background-color: #322c5f;
-  //}
-}
-
-.even-background {
-  background-color: #322c5f;
-}
-
-.tab-index {
-  width: 16px;
-  height: 16px;
-  line-height: 16px;
-
-  text-align: center;
-  border-radius: 50%;
-  background-color: #4666ff;
-}
-
-.index-bg {
-  background-color: #f43737;
-}
-</style>
